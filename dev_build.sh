efa854755d9541d30fe30035cfd6cf6146e31e4a
PraveenGops <153624883+PraveenGops@users.noreply.github.com>
2024-05-16 22:23:51 +0530

    Update and rename build.sh to dev_build.sh

--- dev_build.sh
+++ dev_build.sh
@@ -3,7 +3,9 @@
 echo "building image from docker-compose-file"
 docker-compose build
 docker images
+echo "running a container"
+docker-compose up -d
 echo "pushing the image to hub"
-docker tag Main_project_zenapp praveengdocker/dev_region:image${BUILD_NUMBER}
+docker tag dev_zenapp praveengdocker/dev_region:image${BUILD_NUMBER}
 docker login -u praveengdocker -p G@7160598
 docker push praveengdocker/dev_region:image${BUILD_NUMBER}
